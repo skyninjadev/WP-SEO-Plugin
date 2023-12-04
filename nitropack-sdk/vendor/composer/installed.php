64f69c0351e28fec09078442ed8340819ba6840f
skyninjadev <kingaku0530@gmail.com>
2023-12-05 00:31:06 +0900

    'main'

---
+++ nitropack-sdk/vendor/composer/installed.php
@@ -0,0 +1,45 @@
+<?php return array(
+    'root' => array(
+        'name' => 'nitropack/nitropackcloud-sdk',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => '858d8ad5858d2d275755b70e87c21ab7a426b938',
+        'type' => 'package',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'nitropack/httpclient' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '549b4e269581e105616e880577193db60628a482',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../nitropack/httpclient',
+            'aliases' => array(
+                0 => '9999999-dev',
+            ),
+            'dev_requirement' => false,
+        ),
+        'nitropack/nitropackcloud-sdk' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '858d8ad5858d2d275755b70e87c21ab7a426b938',
+            'type' => 'package',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'nitropack/url' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => 'cea1fae804074ef3ec57f90f3a267600ee65275c',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../nitropack/url',
+            'aliases' => array(
+                0 => '9999999-dev',
+            ),
+            'dev_requirement' => false,
+        ),
+    ),
+);
